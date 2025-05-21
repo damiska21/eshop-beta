1a75b8c818797aa2f89e74d601a7582cc5991c50
Damián Roháček <rohacekdamian@seznam.cz>
2025-05-21 14:54:56 +0200

    čistší routy a hezký menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './pages/Home'
-import Products from './pages/Products'
-import Contact from './pages/Contact'
-import Navbar from './components/common/Navbar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "./App.css";
+import Navbar from "./components/common/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact-us" element={<Contact />} />
-      </Routes>
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
