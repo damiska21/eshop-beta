9663cde918e5d3067eca400536a943d47a8570aa
foxx16xc <jakub.podhajecky@seznam.cz>
2025-05-26 13:51:35 +0200

    Úprava home po lokalizaci a úprava lokalizace

--- src/components/common/HomeC.jsx
+++ src/components/common/HomeC.jsx
@@ -4,13 +4,29 @@ import { Link } from "react-router-dom";
 
 function HomeC() {
   const { strings } = useLocalization();
+  const benefits = strings.home.benefits.items;
+
   return (
-    <>
+    <div className="home-container">
       <h1>{strings.home.h1}</h1>
       <h2>{strings.home.h2}</h2>
       <p>{strings.home.text1}</p>
       <p>{strings.home.text2}</p>
-    </>
+
+      <h2>{strings.home.benefits.title}</h2>
+      <div className="articles-wrapper">
+        {benefits.map((item, index) => (
+          <article key={index}>
+            <h3>{item.title}</h3>
+            <p>{item.text}</p>
+          </article>
+        ))}
+      </div>
+
+      <Link to="/products" className="custom-link">
+        Jdeme nakupovat!!!
+      </Link>
+    </div>
   );
 }
 
