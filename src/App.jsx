fbb99fc0c919f5fc2b68dbe7f77862be8257ad0e
foxx16xc <jakub.podhajecky@seznam.cz>
2025-05-26 10:36:23 +0200

    Úprava Nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Footer from "./components/common/Footer";
 import Navigationbar from "./components/common/Navigationbar";
 
+
 function App() {
   return (
     <>
       <Navigationbar />
- 
+      
       <Footer />
     </>
   );
