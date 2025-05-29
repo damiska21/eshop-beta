21c5570247177d21f910f1ac6b7e3e6903b77f41
foxx16xc <jakub.podhajecky@seznam.cz>
2025-05-29 10:56:03 +0200

    Úprava stránek a lokalizace

--- src/pages/TermsConditions.jsx
+++ src/pages/TermsConditions.jsx
@@ -1,9 +1,11 @@
 import PageWrapper from "../components/common/ui-effects/PageWrapper.jsx";
 import { useLocalization } from "../contexts/LocalizationContext.jsx";
+import "./TermsConditions.css"
 export default function TermsConditions() {
   const { strings } = useLocalization();
   return (
     <PageWrapper>
+      <div className="justify">
       <h1>{strings.termsconditions.title}</h1>
       <h2>{strings.termsconditions.h1}</h2>
       <p>{strings.termsconditions.t1}</p>
@@ -19,6 +21,7 @@ export default function TermsConditions() {
       <p>{strings.termsconditions.t6}</p>
       <h2>{strings.termsconditions.h7}</h2>
       <p>{strings.termsconditions.t7}</p>
+      </div>
     </PageWrapper>
   );
 }
